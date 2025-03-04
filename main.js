1bd005f4884d56ed52810be35e98c68f30498a18
Robbie <Robbie@email.com>
2025-03-05 03:36:00 +0900

    I completed javascript

---
+++ main.js
@@ -0,0 +1,102 @@
+let comNum=0;
+
+let playButton = document.getElementById("play-button");
+let userInput = document.getElementById("user-input");
+let resultArea = document.getElementById("result-area");
+let resetButton = document.getElementById("reset-button");
+let chanceInfo = document.getElementById("chance-info");
+let chances=5;
+let go=0;
+let history=[]
+
+
+
+
+playButton.addEventListener("click",play);
+resetButton.addEventListener("click",re);
+userInput.addEventListener("focus",function(){userInput.value=""});
+
+console.log(playButton);
+
+while (true){
+chances=Number(prompt("몇 번 시도할까요? (1~100 사이의 숫자를 입력해주세요)",100));
+if ((chances>0)&&(chances<=200))
+{
+    break;
+}
+}
+function mkRanNum(){
+    comNum=Math.floor(Math.random()*100)+1;
+    console.log("랜덤값: ", comNum);
+}
+chanceInfo.textContent="남은횟수: "+(chances-go);
+
+function play(){
+    
+    console.log("남은 횟수",chances-go);
+    let userValue=userInput.value;
+
+    if((userValue>100)||(userValue<1))
+    {
+        if(userValue>100){
+            console.log("100 이하의 수를 입력하세요");
+            alert("100이하의 수를 입력하세요");
+            return 0;
+        }
+        if(userValue<1){
+            console.log("1 이상의 수를 입력하세요");
+            alert("1이상의 수를 입력하세요");
+            return 0;
+        }
+    }
+    if(history.includes(userValue)){
+        alert("이미 입력한 값 입니다. 다른 값을 입력하세요");
+        return 0;
+    }
+    history.push(userValue);
+    console.log(history);
+    go++;
+    if (userValue<comNum){
+        resultArea.textContent="UP";
+        chanceInfo.textContent="남은횟수: "+(chances-go);
+        console.log("UP!! 남은횟수:");
+
+    }else if(userValue>comNum){
+        resultArea.textContent="DOWN";
+        chanceInfo.textContent="남은횟수: "+(chances-go);
+        console.log("Down!");
+    }else{
+        resultArea.textContent="맞췄습니다.!";
+        chanceInfo.textContent="남은횟수: "+(chances-go);
+        console.log("맟췄습니다");
+        playButton.disabled=true;
+    }
+    if (go== chances)
+        {
+            playButton.disabled=true;
+            resultArea.textContent="실패...";
+        }
+    
+    
+}
+
+function re(){
+   
+    while (true){
+        chances=Number(prompt("몇 번 시도할까요? (1~100 사이의 숫자를 입력해주세요)",100));
+        if ((chances>0)&&(chances<=200))
+        {
+            break;
+        }
+        }
+    resultArea.textContent="게임을 시작합니다"
+    go=0;
+    chanceInfo.textContent="남은횟수: "+(chances-go);
+    playButton.disabled=false;
+    userInput.value="";
+    mkRanNum();
+
+
+}
+
+mkRanNum();
